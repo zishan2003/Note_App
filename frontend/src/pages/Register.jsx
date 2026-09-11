22281a87ff10c302b0b31cf52eb118f1b023d7b1
zishan hoda <zishanhoda99@gmail.com>
2026-09-11 16:10:17 +0530

    Responsive

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./Register.css";
 
 const Register = () => {
   const [name, setName] = useState("");
